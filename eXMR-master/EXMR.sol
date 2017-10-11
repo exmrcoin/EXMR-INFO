662e785125c062b759cfe340962e9c9512d028cb
e-XMR <postmaster@e-xmr.io>
2017-10-11 04:48:15 -0400

    Update Mint eXMR

--- eXMR-master/EXMR.sol
+++ eXMR-master/EXMR.sol
@@ -1,143 +1,248 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.16;
 
-/*
-* EXMR 'eXMR eMonero is tokenized (ERC20) Monero on the Ethereum blockchain' 
+/* EXMR 'eXMR eMonero is tokenized (ERC20) Monero on the Ethereum blockchain' 
 *Airdrop  100% Free  contract Version 2.0
-* Refer to our Official Sites and your Community: 
+*Refer to our Official Sites and your Community: 
 *http://www.e-XMR.io/
 *https://twitter.com/eXMRCoin
 *https://medium.com/@eXMR
 *https://www.reddit.com/user/exmrcoin
 *https://github.com/eXMRcoin/e-XMR/tree/master/eXMR-master
 *https://t.me/joinEXMR
-* 
+*
 * Developer: eXMR (TM) 2017.
 *
-* 
-* 
 * The MIT License.
 *
 */
+    library SafeMath {
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    contract ERC20 {
-     function totalSupply() constant returns (uint256 totalSupply);
-     function balanceOf(address _owner) constant returns (uint256 balance);
-     function transfer(address _to, uint256 _value) returns (bool success);
-     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-     function approve(address _spender, uint256 _value) returns (bool success);
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
- }
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
 
-  contract EXMR is ERC20 {
-     string public constant symbol = "EXMR";
-     string public constant name = "eXMR";
-     uint8 public constant decimals = 8;
-     uint256 _totalSupply = 15000000 * 10**8;
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
-     address public owner;
-
-     mapping(address => uint256) balances;
-
-     mapping(address => mapping (address => uint256)) allowed;
+contract Ownable {
+  address public owner;
 
 
-     function EXMR() {
-         owner = msg.sender;
-         balances[owner] = 15000000 * 10**8;
-     }
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
 
-     modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      owner = newOwner;
     }
+  }
+
+}
+
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant returns (uint256);
+  function transfer(address to, uint256 value) returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _value The amount to be transferred.
+  */
+  function transfer(address _to, uint256 _value) returns (bool) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of. 
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
 
 
-    function distributeEXMRLarge(address[] addresses) onlyOwner {
-        for (uint i = 0; i < addresses.length; i++) {
-            balances[owner] -= 750879664000;
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _value uint256 the amout of tokens to be transfered
+   */
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+    var _allowance = allowed[_from][msg.sender];
 
-            require(balances[owner] >= 0);
+    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
+    // require (_value <= _allowance);
 
-            balances[addresses[i]] += 750879664000;
-            Transfer(owner, addresses[i], 750879664000);
-        }
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) returns (bool) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifing the amount of tokens still avaible for the spender.
+   */
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+}
+
+contract MintableToken is StandardToken, Ownable {
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+
+  bool public mintingFinished = false;
+
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will recieve the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
+}
+
+contract EXMRO is MintableToken {
+  string public constant name = "eXMR";
+  string public constant symbol = "EXMRO";
+  uint   public constant decimals = 8;
+  uint   public unlockTimeStamp = 0;  
+
+  mapping (address => bool) private _lockByPass;
+  
+  function EXMRO(uint unlockTs){
+    setUnlockTimeStamp(unlockTs);
+  }
+
+  function setUnlockTimeStamp(uint _unlockTimeStamp) onlyOwner {
+    unlockTimeStamp = _unlockTimeStamp;
+  }
+
+  function airdrop(address[] addresses, uint amount) onlyOwner{
+    require(amount > 0);
+    for (uint i = 0; i < addresses.length; i++) {
+       super.transfer(addresses[i], amount);
     }
+  }
 
-    function distributeEXMRMedium(address[] addresses) onlyOwner {
-        for (uint i = 0; i < addresses.length; i++) {
-            balances[owner] -= 481439832000;
+  function transfer(address _to, uint _value) returns (bool success) {
+    if (now < unlockTimeStamp && !_lockByPass[msg.sender]) return false;
+    return super.transfer(_to, _value);
+  }
 
-            require(balances[owner] >= 0);
+  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+    if (now < unlockTimeStamp && !_lockByPass[_from]) return false;
+    return super.transferFrom(_from, _to, _value);
+  }
 
-            balances[addresses[i]] += 481439832000;
-            Transfer(owner, addresses[i], 481439832000);
-        }
+  function setLockByPass(address[] addresses, bool locked) onlyOwner{
+    for (uint i = 0; i < addresses.length; i++) {
+       _lockByPass[addresses[i]] = locked;
     }
-
-    function distributeEXMRSmall(address[] addresses) onlyOwner {
-        for (uint i = 0; i < addresses.length; i++) {
-            balances[owner] -= 265719916000;
-
-            require(balances[owner] >= 0);
-
-            balances[addresses[i]] += 265719916000;
-            Transfer(owner, addresses[i], 265719916000);
-        }
-    }
-
-
-     function totalSupply() constant returns (uint256 totalSupply) {
-         totalSupply = _totalSupply;
-     }
-
-
-     function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-     }
-
-     function transfer(address _to, uint256 _amount) returns (bool success) {
-         if (balances[msg.sender] >= _amount
-            && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(msg.sender, _to, _amount);
-            return true;
-         } else {
-             return false;
-         }
-     }
-
-
-     function transferFrom(
-         address _from,
-         address _to,
-         uint256 _amount
-     ) returns (bool success) {
-         if (balances[_from] >= _amount
-             && allowed[_from][msg.sender] >= _amount
-             && _amount > 0
-             && balances[_to] + _amount > balances[_to]) {
-             balances[_from] -= _amount;
-             allowed[_from][msg.sender] -= _amount;
-             balances[_to] += _amount;
-             Transfer(_from, _to, _amount);
-             return true;
-         } else {
-            return false;
-         }
-     }
-
-     function approve(address _spender, uint256 _amount) returns (bool success) {
-         allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-         return true;
-     }
-
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-         return allowed[_owner][_spender];
-    }
+  }
 }
